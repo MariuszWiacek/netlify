b88a356ca04c5868f854099dd5c1aacad2045cfd
MariuszWiacek <wiacekm.uber@gmail.com>
2023-03-10 18:31:32 +0000

    update

--- got-netlify-evening/src/components/Character.js
+++ got-netlify-evening/src/components/Character.js
@@ -1,9 +1,44 @@
 import React from "react";
+import Character from "./Character";
 
-const Character = () => {
+import peopleInfo from "../data/characterData.json";
+
+const CharacterGallery = () => {
+    const characterComponentArray = peopleInfo.map(
+        c => <Character {...c} key={c._id} />
+    )
+
+    return (
+        <div data-test="component-char-gallery">
+            {characterComponentArray}
+
+        </div>
+    )
+}
+
+export default CharacterGallery;
+9:01
+import React from "react";
+
+// const Character = props => {
+//     const { name, imgUrl, birth, death, race, realm, spouse } = props;
+const Character = ({ name, imgUrl, birth, death, race, realm, spouse }) => {
+    
+    // const thisCharacterData = props;
+    // const name = thisCharacterData.name;
+
+
     return (
         <div data-test="component-character">
-            
+            <h1 data-test='char-name'>{name}</h1>
+            <img src={imgUrl} alt={name} data-test='char-img' />
+            <ul data-test='char-list'>
+                <li data-test='char-birth'>Date of Birth: {birth}</li>
+                <li data-test='char-death'>Date of Death: {death}</li>
+                <li data-test='char-race'>Race: {race}</li>
+                <li data-test='char-realm'>Realm: {realm}</li>
+                <li data-test='char-spouse'>Spouse: {spouse}</li>
+            </ul>
         </div>
     )
 }
